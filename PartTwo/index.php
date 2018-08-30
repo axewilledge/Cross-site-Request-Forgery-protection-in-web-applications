de482fa88f3cfb53ddd8bc3e9f42611c0e17a0d1
Anjana S Porawagama <axewilledge123@gmail.com>
2018-08-30 15:37:52 +0530

    Initial commit

---
+++ PartTwo/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Assignment 1 - SSD</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h2>Assignment 1 Part One - Synchronizer Token Pattern</h2>
+</div>
+  
+<div class="container">
+  <div class="row">
+   
+  </div>
+</div>
+
+</body>
+</html>
